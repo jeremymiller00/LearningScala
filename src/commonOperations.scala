e296b7ef4fb388d2321c91dd5867921197c72af3
Jeremy Miller <jeremymiller00@gmail.com>
2019-12-31 11:07:47 -0800

    add common operations

---
+++ src/commonOperations.scala
@@ -0,0 +1,11 @@
+println("Some operations are common to all types.")
+5.asInstanceOf[Long]
+5.asInstanceOf[String]
+(7/5).getClass
+(5.0).isInstanceOf[Float]
+"A".hashCode
+20.toByte
+47.toFloat
+(3.0/4.0).toString
+println("Avoid asInstanceOf if possible. Use to<type>.)
+
