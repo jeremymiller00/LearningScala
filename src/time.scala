bd1380b9853cd54625c22d3c3fa7bfde06d28a7e
Jeremy Miller <jeremy.miller@clarivate>
2021-12-21 16:43:22 -0800

    cleanup commit

---
+++ src/time.scala
@@ -0,0 +1,9 @@
+def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+    result
+}
+
+var list = time {List.range(1,1000, 1)} 
